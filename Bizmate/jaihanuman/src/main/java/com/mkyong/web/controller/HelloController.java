9c71785e5aae40c8e95816cebf8b456eef068120
vijayexist@gmailcom <Welcome123#>
2015-10-01 22:29:59 -0700

    bizmate initial commit

---
+++ Bizmate/jaihanuman/src/main/java/com/mkyong/web/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.mkyong.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/display2", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		return "dashboard";
+
+	}
+
+	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+	public ModelAndView hello(@PathVariable("name") String name) {
+
+		ModelAndView model = new ModelAndView();
+		model.setViewName("Dashboard");
+		model.addObject("msg", name);
+
+		return model;
+
+	}
+
+}
